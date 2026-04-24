234ab5dc01eb6a7a86f76a1813b147c5cc14dfe6
valentyn <nvd3102@gmail.com>
2026-04-24 20:12:12 +0200

    fix api url

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: 'https://tracker-production-c9ec.up.railway.app/api' });
 
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('token');
